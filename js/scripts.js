81b126e7d98172be92263a66245aa70906fba402
Cristina Lester <cristina.deelester@gmail.com>
2022-02-18 10:42:32 -0800

    added responsive styles to modal body

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,33 @@
 $(document).ready(() => {
+  //opening modal
   $("#open-modal-btn").on("click", () => {
     $("#video-modal").fadeIn("slow", "swing").addClass("opened");
   });
 
+  //closing modal when clicking on close button
   $("#close-modal-btn").on("click", (event) => {
     event.stopPropagation();
-    $("#video-modal").fadeOut("slow", "swing", function () {
-      $(this).removeClass("opened");
-    });
+    closeModal();
   });
 
-  // $("#close-modal-btn").on("click", (event) => {
-  //   event.stopPropagation();
-  //   $("#video-modal").removeClass("opened");
-  // });
+  //closing modal when pushing escape key
+  $(document).on("keydown", (event) => {
+    //keyCode = 27 is escape key
+    if (event.keyCode == 27) {
+      closeModal();
+    }
+  });
 
-  // $("#video-modal").click(function (event) {
-  //   event.stopPropagation();
-  //   $(this).removeClass("opened");
-  // });
+  //closing modal when clicking outside of its body
+  $("#video-modal").on("click", function (event) {
+    if (event.target === this) {
+      closeModal();
+    }
+  });
 });
+
+function closeModal() {
+  $("#video-modal").fadeOut("slow", "swing", function () {
+    $(this).removeClass("opened");
+  });
+}
