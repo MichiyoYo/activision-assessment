16fc54f881b80620959cbb084f7dac627cc6f9c8
Cristina Lester <cristina.deelester@gmail.com>
2022-02-18 10:23:39 -0800

    added fadeIn and fadeOut on modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
 $(document).ready(() => {
   $("#open-modal-btn").on("click", () => {
-    $("#video-modal").addClass("opened");
+    $("#video-modal").fadeIn("slow", "swing").addClass("opened");
   });
 
-  $("#close-modal-btn, #video-modal").on("click", () => {
-    $("#video-modal").removeClass("opened");
+  $("#close-modal-btn").on("click", (event) => {
+    event.stopPropagation();
+    $("#video-modal").fadeOut("slow", "swing", function () {
+      $(this).removeClass("opened");
+    });
   });
+
+  // $("#close-modal-btn").on("click", (event) => {
+  //   event.stopPropagation();
+  //   $("#video-modal").removeClass("opened");
+  // });
+
+  // $("#video-modal").click(function (event) {
+  //   event.stopPropagation();
+  //   $(this).removeClass("opened");
+  // });
 });
